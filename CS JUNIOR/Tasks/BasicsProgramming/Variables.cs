d2dd5e13fd2f3e3e84872f70d4809eb05c4279da
Kislota_RU <tsepilov.matvey@gmail.com>
2024-07-22 17:50:31 +0300

    Moving files

---
+++ CS JUNIOR/Tasks/BasicsProgramming/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kislota_RU";
+            char coinSymbol = '$';
+            long moneyCount = 8;
+            uint enemyCount = 3;
+            int medicalKitCount = 0;
+            byte medicalKitTreatment = 70;
+            short cupcakePrice = 6;
+            float healthPlayer = 100f;
+            decimal timeReloadingWeapon = 7.5m;
+            double damageTrap = 30;
+            bool isAlivePlayer = true;
+            bool isDamaged = false;
+        }
+    }
+}
---
+++ CS JUNIOR/Tasks/BasicsProgramming/WorkingWithStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class WorkingWithStrings
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Какой ваш любимый фильм: ");
+            string titleMovie = Console.ReadLine();
+
+            Console.Write("Где вы работаете: ");
+            string workPlace = Console.ReadLine();
+
+            Console.Write("Какой ваш рост в см: ");
+            string growth = Console.ReadLine();
+
+            Console.Write("Какой ваш возраст: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"\nВас зовут {name} и вам {age}. Также ваш рост состовляет " +
+                              $"{growth} см.\nВаш любимый фильм называется \"{titleMovie}\" и вы работаете {workPlace}.\n");
+        }
+    }
+}
