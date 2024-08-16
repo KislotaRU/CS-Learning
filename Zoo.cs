209fc8458654156b2864cb166da08d7e3067d487
Kislota_RU <tsepilov.matvey@gmail.com>
2024-08-16 17:16:48 +0300

    Update Task 'Zoo'

--- Zoo.cs
+++ Zoo.cs
@@ -8,18 +8,143 @@ using System.Collections.Generic;
 
 namespace CS_JUNIOR
 {
-    class Zoo
+    class Program
     {
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.White;
 
+            Zoo zoo = new Zoo();
 
+            zoo.Work();
         }
     }
 }
 
-abstract class Enclosure
+static class UserUtils
 {
+    private static Random s_random;
 
+    static UserUtils()
+    {
+        s_random = new Random();
+    }
+
+    public static int GenerateRandomNumber(int minNumber, int maxNumber)
+    {
+        return s_random.Next(minNumber, maxNumber);
+    }
+
+    public static int GenerateRandomNumber(int maxNumber)
+    {
+        return s_random.Next(maxNumber);
+    }
+}
+
+class Zoo
+{
+    private const string CommandT = "Подойти к вольеру с животными";
+    private const string CommandExit = "Выйти";
+
+    private readonly List<Enclosure> _enclosures;
+
+    public Zoo()
+    {
+        _enclosures = new List<Enclosure>();
+    }
+
+    public void Work()
+    {
+        string[] main = new string[]
+        {
+            CommandT,
+            CommandExit
+        };
+
+        string userInput = null;
+
+        while (userInput != CommandExit)
+        {
+
+
+            switch (userInput)
+            {
+                case CommandT:
+
+                    break;
+
+                case CommandExit:
+
+                    break;
+
+                default:
+                    Console.Write("Ввод не соответствует номеру команды.");
+                    break;
+            }
+        }
+    }
+}
+
+class Enclosure
+{
+    private readonly List<Animal> _animals;
+
+    public Enclosure()
+    {
+        _animals = new List<Animal>(); 
+    }
+
+    public int AnimalsCount { get { return _animals.Count; } }
+}
+
+abstract class Animal
+{
+    public string Name { get ; protected set; }
+    public string Sounds { get; protected set; }
+    public string Gender { get; protected set; }
+
+    public void Show()
+    {
+        Console.Write($"{Name} ");
+    }
+}
+
+class Bear : Animal
+{
+    private readonly string _name = "Медведь";
+
+    public Bear()
+    {
+        Name = _name;
+    }
+}
+
+class Wolf : Animal
+{
+    private readonly string _name = "Волк";
+
+    public Wolf()
+    {
+        Name = _name;
+    }
+}
+
+class Capybara : Animal
+{
+    private readonly string _name = "Капибара";
+
+    public Capybara()
+    {
+        Name = _name;
+    }
+}
+
+class Penguin : Animal
+{
+    private readonly string _name = "Пингвин";
+
+    public Penguin()
+    {
+        Name = _name;
+    }
 }
