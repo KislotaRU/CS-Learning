cab6e4a8b7dd683b763fe43470e01857965065f6
Kislota_RU <tsepilov.matvey@gmail.com>
2024-08-30 21:50:01 +0300

    Add and Update Task 'SwappingValues'

---
+++ SwappingValues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * Даны две переменные. Поменять местами значения двух переменных. 
+ * Вывести на экран значения переменных до перестановки и после.
+*/
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string firstName = "Гоголь";
+            string lastName = "Николай";
+
+            string temporaryName; 
+
+            Console.Write("Данные до.\n" +
+                          $"Имя: {firstName}\n" +
+                          $"Фамилия: {lastName}\n");
+
+            temporaryName = firstName;
+            firstName = lastName;
+            lastName = temporaryName;
+
+            Console.Write("\nДанные после.\n" +
+                          $"Имя: {firstName}\n" +
+                          $"Фамилия: {lastName}\n");
+        }
+    }
+}
