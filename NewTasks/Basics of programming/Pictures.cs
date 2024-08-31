0c29809a538bc09acacde1443f7e317e2a016801
Kislota_RU <tsepilov.matvey@gmail.com>
2024-08-31 14:31:51 +0300

    Relocated Task 'Pictures' in Folder 'Basics of programming'

---
+++ NewTasks/Basics of programming/Pictures.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * На экране, в специальной зоне, выводятся картинки, по 3 в ряд (условно, ничего рисовать не надо).
+ * Всего у пользователя в альбоме 52 картинки. Код должен вывести, сколько полностью заполненных
+ * рядов можно будет вывести, и сколько картинок будет сверх меры.
+*/
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            int picturesCount = 52;
+            int maxCountPicturesInRow = 3;
+
+            int rowsFilledCount = picturesCount / maxCountPicturesInRow;
+            int remainderPictures = picturesCount % maxCountPicturesInRow;
+
+            Console.Write($"Кол-во заполненных рядов: {rowsFilledCount}\n" +
+                          $"Кол-во оставшихся картинок: {remainderPictures}\n");
+        }
+    }
+}
