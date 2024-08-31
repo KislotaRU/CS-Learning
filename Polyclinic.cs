083581f38d1894b3e1acd04e49cfdafc49b9ab8a
Kislota_RU <tsepilov.matvey@gmail.com>
2024-08-31 15:12:14 +0300

    Add and Update Task 'Polyclinic'

---
+++ Polyclinic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/*
+ * Пользователь вводит кол-во людей в очереди.
+ * Фиксированное время приема одного человека всегда равно 10 минутам.
+ * Пример ввода: Введите кол-во пациентов: 14
+ * Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут."
+*/
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            int hourInMinutes = 60;
+
+            int AppointmentDurationInMinutes = 10;
+
+            int queueTimeInMinutes;
+            int hoursCount;
+            int minutesCount;
+
+            int peopleCount;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("\tПоликлиника Новикова\n");
+
+            Console.Write($"Вам надо попасть на приём к врачу. Приём занимает {AppointmentDurationInMinutes} минут.\n");
+
+            Console.Write("Введите кол-во людей, стоящие перед вами в очереди: ");
+            peopleCount = Convert.ToInt32(Console.ReadLine());
+
+            queueTimeInMinutes = peopleCount * AppointmentDurationInMinutes;
+
+            hoursCount = queueTimeInMinutes / hourInMinutes;
+            minutesCount = queueTimeInMinutes % hourInMinutes;
+
+            Console.Write($"\nВаша очередь подойдёт через кол-во часов {hoursCount} и минут {minutesCount}\n");
+        }
+    }
+}
