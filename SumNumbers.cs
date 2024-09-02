95fd2bbd07a92315ba5e8d4e53b38ca2cb1002c6
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-02 12:56:33 +0300

    Add and Update Task 'SumNumbers'

---
+++ SumNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int maxNumber = 100;
+            int randomNumber;
+
+            int firstDivider = 3;
+            int secondDivider = 5;
+
+            int sumNumbers = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            randomNumber = random.Next(maxNumber + 1);
+
+            Console.Write($"Рандомное максимальное число: {randomNumber}\n");
+
+            for (int i = 0; i <= randomNumber; i++)
+            {
+                if ((i % firstDivider == 0) && (i % secondDivider == 0))
+                    sumNumbers += i;
+            }
+
+            Console.Write($"Сумма чисел: {sumNumbers}\n");
+        }
+    }
+}
