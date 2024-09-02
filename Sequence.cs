24dc8bcd68b41b80d4a9b3becbd122d4a8c53f7a
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-02 12:26:10 +0300

    Add and Update Task 'Sequence'

---
+++ Sequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            int stepSize = 7;
+            int minNumber = 5;
+
+            int maxNumber;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Программа выводит последовательность чисел до указанного максимума.\n\n");
+
+            Console.Write("Введите максимальное число: ");
+            maxNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = minNumber; i <= maxNumber; i += stepSize)
+                Console.Write($"{i}\n");
+        }
+    }
+}
