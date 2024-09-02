a0ab6c6f52b5357a9ec9b20a576764715a7edce2
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-02 11:28:11 +0300

    Add and Update Task 'MasteringCycles'

---
+++ MasteringCycles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Напишите простейшую программу, которая выводит указанное(установленное) пользователем сообщение заданное количество раз. 
+ * Количество повторов также должен ввести пользователь.
+*/
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userMessage;
+            int repetitionsCount;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Программа выводит ваше сообщение указанное вами кол-во раз.\n\n");
+
+            Console.Write("Введите сообщение: ");
+            userMessage = Console.ReadLine();
+
+            Console.Write("Введите число повторений: ");
+            repetitionsCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repetitionsCount; i++)
+                Console.Write($"{i + 1}. {userMessage}\n");
+        }
+    }
+}
