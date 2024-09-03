5d19d538eb36193ba9db635ed8ef27e114f100ce
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-03 13:43:51 +0300

    Add and update Task 'DegreeTwo'

---
+++ DegreeTwo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int maxNumber = 10000;
+            int randomNumber;
+
+            int degreeNumber = 0;
+            int number = 2;
+            int result = 1;
+
+            randomNumber = random.Next(maxNumber);
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write($"Заданно число: {randomNumber}\n");
+
+            while (randomNumber > result)
+            {
+                degreeNumber++;
+                result *= number;
+            }
+
+            Console.Write($"{number} в степени {degreeNumber}, что явялеется числом {result} превосходит заданное число {randomNumber}.\n");
+        }
+    }
+}
