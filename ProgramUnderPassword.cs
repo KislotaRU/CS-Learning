74aa8a1402925d7347c1c7658facb86bc2ca8b63
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-03 13:09:17 +0300

    Update Task 'ProgramUnderPassword'

---
+++ ProgramUnderPassword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string password = "28743W!IUwulW@";
+            int attemptsCount = 3;
+
+            string userInput;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Программа хранит секрет. Его можно получить введя верный пароль.\n\n");
+
+            for (int i = attemptsCount; i > 0; i--)
+            {
+                Console.Write($"Попыток осталось: {i}\n");
+
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                    Console.Write("Пароль верный. Вам доступен секрет.\n\n");
+                else
+                    Console.Write("Пароль неверный.\n\n");
+            }
+        }
+    }
+}
