a16009d844c0c9294abbce6f68b7011076b3af89
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-03 12:19:18 +0300

    Update Task 'NameOutput'

--- NameOutput.cs
+++ NameOutput.cs
@@ -6,8 +6,30 @@ namespace CS_JUNIOR
     {
         static void Main()
         {
+            int frameLength = 2;
+
+            string userName;
+            char frameSign;
+
             Console.ForegroundColor = ConsoleColor.White;
 
+            Console.Write("Введите ваше имя: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Введите символ обводки вашего имени: ");
+            frameSign = Convert.ToChar(Console.ReadLine());
+
+            frameLength += userName.Length;
+
+            for (int i = 0; i < frameLength; i++)
+                Console.Write(frameSign);
+
+            Console.Write($"\n{frameSign}{userName}{frameSign}\n");
+
+            for (int i = 0; i < frameLength; i++)
+                Console.Write(frameSign);
+
+            Console.WriteLine();
         }
     }
 }
