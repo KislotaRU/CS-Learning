4dc0bc4d0393f5309473b3fef81e067c438af72c
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-05 15:26:30 +0300

    Add and update Task 'Split'

---
+++ Split.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sentence = "На ваше имя пришло письмо. Прочитать его?";
+
+            string[] words;
+            char separator = ' ';
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            words = sentence.Split(separator);
+
+            for (int i = 0; i < words.Length; i++)
+                Console.Write($"{words[i]}\n");
+        }
+    }
+}
