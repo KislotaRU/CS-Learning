9ef5dcadcc6bf3e97dfd534b13b04d10d2250f1b
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-05 18:10:40 +0300

    Update Task 'ShiftingArrayValues'

--- ShiftingArrayValues.cs
+++ ShiftingArrayValues.cs
@@ -6,7 +6,42 @@ namespace CS_JUNIOR
     {
         static void Main()
         {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int shiftLength;
+            int lastIndex = numbers.Length - 1;
+
             Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Программа сдвигает числа в массиве влево на указанное число позиций.\n");
+
+            Console.Write("Массив:\n\n");
+
+            foreach (int number in numbers)
+                Console.Write($"{number} ");
+
+            Console.Write("\n\nВведите число: ");
+
+            shiftLength = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < shiftLength; i++)
+            {
+                int temporaryNumber;
+
+                for (int j = 0; j < lastIndex; j++)
+                {
+                    temporaryNumber = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temporaryNumber;
+                }
+            }
+
+            Console.Write("\n\n");
+
+            foreach (int number in numbers)
+                Console.Write($"{number} ");
+
+            Console.Write("\n\n");
         }
     }
 }
