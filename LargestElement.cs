383b50cd71ffc4d9333c45deadd83b89d554a86e
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-05 12:00:36 +0300

    Update Task 'LargestElement'

--- LargestElement.cs
+++ LargestElement.cs
@@ -14,6 +14,8 @@ namespace CS_JUNIOR
             int rowsCount = 10;
             int colomnsCount = 10;
 
+            int replacement = 0;
+
             int[,] arrayNumber = new int[rowsCount, colomnsCount];
 
             int largestNumberArray = int.MinValue;
@@ -28,22 +30,22 @@ namespace CS_JUNIOR
                 {
                     arrayNumber[i, j] = random.Next(minNumber, maxNumber);
                     Console.Write($"{arrayNumber[i, j]} ");
+
+                    if (largestNumberArray < arrayNumber[i, j])
+                        largestNumberArray = arrayNumber[i, j];
                 }
 
                 Console.WriteLine();
             }
 
-            Console.Write($"Изменённая матрица A:\n\n");
+            Console.Write($"\nИзменённая матрица A:\n\n");
 
             for (int i = 0; i < arrayNumber.GetLength(0); i++)
             {
                 for (int j = 0; j < arrayNumber.GetLength(1); j++)
                 {
-                    if (largestNumberArray < arrayNumber[i, j])
-                    {
-                        largestNumberArray = arrayNumber[i, j];
-                        arrayNumber[i, j] = 0;
-                    }
+                    if (largestNumberArray == arrayNumber[i, j])
+                        arrayNumber[i, j] = replacement;
 
                     Console.Write($"{arrayNumber[i, j]} ");
                 }
