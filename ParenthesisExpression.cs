589d3c1b4f95e678742fec0db5dd6235f6ae2de4
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-05 18:57:00 +0300

    Add and update Task 'ParenthesisExpression'

---
+++ ParenthesisExpression.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            char parenthesisLeft = '(';
+            char parenthesisRight = ')';
+
+            string line;
+
+            int maxDepthLine = 0;
+            int parenthesisCount = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Введите скобочное выражение: ");
+            line = Console.ReadLine();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == parenthesisLeft)
+                    parenthesisCount++;
+                else if (line[i] == parenthesisRight)
+                    parenthesisCount--;
+
+                if (maxDepthLine < parenthesisCount)
+                    maxDepthLine = parenthesisCount;
+
+                if (parenthesisCount < 0)
+                    break;
+            }
+
+            if (parenthesisCount == 0)
+                Console.Write($"\nСкобочное выражение корректное, максимальная глубина составляет {maxDepthLine}.\n\n");
+            else
+                Console.Write($"\nСкобочное выражение некорректное.\n\n");
+        }
+    }
+}
