f35508c823e08d20f90f2734e3683b3a4160fd6e
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-06 19:52:26 +0300

    Update Task 'WorkingWithSpecificRowsAndColumns'

--- WorkingWithSpecificRowsAndColumns.cs
+++ WorkingWithSpecificRowsAndColumns.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+/*
+ * Дан двумерный массив.
+ * Вычислить сумму второй строки и произведение первого столбца.
+ * Вывести исходную матрицу и результаты вычислений. 
+*/
+
 namespace CS_JUNIOR
 {
     class Program
@@ -13,7 +19,7 @@ namespace CS_JUNIOR
             int rowsCount = 4;
             int columnsCount = 4;
 
-            int[,] arrayNumber = new int[rowsCount, columnsCount];
+            int[,] numbers = new int[rowsCount, columnsCount];
 
             int rowIndex = 2;
             int columnIndex = 1;
@@ -25,25 +31,25 @@ namespace CS_JUNIOR
 
             Console.Write($"Двумерный массив:\n\n");
 
-            for (int i = 0; i < arrayNumber.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                for (int j = 0; j < arrayNumber.GetLength(1); j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    arrayNumber[i, j] = random.Next(maxNumber);
-                    Console.Write($"{arrayNumber[i, j]} ");
+                    numbers[i, j] = random.Next(maxNumber);
+                    Console.Write($"{numbers[i, j]} ");
                 }
 
                 Console.WriteLine();
             }
 
-            for (int j = 0; j < arrayNumber.GetLength(1); j++)
+            for (int j = 0; j < numbers.GetLength(1); j++)
             {
-                sum += arrayNumber[rowIndex - 1, j];
+                sum += numbers[rowIndex - 1, j];
             }
 
-            for (int i = 0; i < arrayNumber.GetLength(0); i++)
+            for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                product *= arrayNumber[i, columnIndex - 1];
+                product *= numbers[i, columnIndex - 1];
             }
 
             Console.Write($"\nСумма чисел строки под номером {rowIndex} = {sum}\n" +
