30d0d7bcb0f4417fb67628a14ac248a152ed118b
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-07 11:11:18 +0300

    Update Task 'ReadInt'

--- ReadInt.cs
+++ ReadInt.cs
@@ -6,8 +6,28 @@ namespace CS_JUNIOR
     {
         static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.White;
+            int number;
 
+            Console.Write("Программа выводит введённое число.\n\n");
+
+            number = ReadInt();
+
+            Console.Write($"Вы ввели число {number}\n");
+        }
+
+        static int ReadInt()
+        {
+            int number;
+            string userInput;
+
+            do
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+            }
+            while (int.TryParse(userInput, out number) == false);
+
+            return number;
         }
     }
 }
