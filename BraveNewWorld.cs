b8c474642da8f7a6e7316d1b261886c456d80ed9
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-07 17:52:59 +0300

    Add and update Task 'BraveNewWord'

---
+++ BraveNewWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[,] map = null;
+
+            string puth = "BraveNewWordMap";
+
+            ReadMap(puth, ref map);
+        }
+
+        static void ReadMap(string path, ref string[,] map)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            for (int x = 0; x < lines.Length; x++)
+                for (int y = 0; y < lines.Length; y++)
+                    map[y, y] = lines[y];
+        }
+
+        static void DrawMap()
+        {
+
+        }
+    }
+}
