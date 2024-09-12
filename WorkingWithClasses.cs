e6796ddd023d42f373ceb75ac6db027020659ea9
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-12 19:45:10 +0300

    Add and update Task 'WorkingWithClasses'

---
+++ WorkingWithClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Кощей";
+
+            Player player = new Player(name);
+
+            player.ShowInfo();
+        }
+    }
+}
+
+class Player
+{
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+    public void ShowInfo()
+    {
+        Console.Write($"Имя: {Name}");
+    }
+}
