58fca0e0141a2957d79e504a85af2d5166d84fc6
Kislota_RU <tsepilov.matvey@gmail.com>
2024-09-13 19:36:52 +0300

    Add and update Task 'WorkingWithProperties'

---
+++ WorkingWithProperties.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            char symbol = '@';
+
+            int positionX = 25;
+            int positionY = 10;
+
+            Player player = new Player(symbol, positionX, positionY);
+            Render render = new Render();
+
+            Console.CursorVisible = false;
+
+            render.DrawPlayer(player);
+
+            Console.ReadKey(true);
+        }
+    }
+}
+
+class Player
+{
+    public Player(char symbol, int positionX, int positionY)
+    {
+        Symbol = symbol;
+        PositionX = positionX;
+        PositionY = positionY;
+    }
+
+    public char Symbol { get; private set; }
+    public int PositionX { get; private set; }
+    public int PositionY { get; private set; }
+}
+
+class Render
+{
+    public void DrawPlayer(Player player)
+    {
+        Console.SetCursorPosition(player.PositionX, player.PositionY);
+        Console.Write(player.Symbol);
+    }
+}
