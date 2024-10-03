aefa6ae77d809fd8f4b4d349a188b59760ae3753
Kislota_RU <tsepilov.matvey@gmail.com>
2024-10-03 13:01:18 +0300

    Add and update Task 'Amnesty'

---
+++ Amnesty.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS_JUNIOR
+{
+    class Program
+    {
+        static void Main()
+        {
+            CriminalDatabase criminalDataBase = new CriminalDatabase();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            criminalDataBase.Work();
+        }
+    }
+}
+
+public static class UserUtils
+{
+    public static int ReadInt()
+    {
+        int number;
+
+        string userInput = Console.ReadLine();
+
+        while (int.TryParse(userInput, out number) == false)
+        {
+            Console.Write("Требуется ввести число: ");
+            userInput = Console.ReadLine();
+        }
+
+        return number;
+    }
+}
+
+public class CriminalDatabase
+{
+    private readonly List<Criminal> _criminals;
+
+    public CriminalDatabase()
+    {
+        _criminals = new List<Criminal>()
+        {
+            new Criminal("Блохин", "Юстиниан", "Петрович", true),
+            new Criminal("Беляев", "Лев", "Станиславович", false, "Антиправительственное"),
+            new Criminal("Тимофеева", "Алёна", "Федосеевна", false),
+            new Criminal("Семёнова", "Серафима", "Иринеевна", true, "Антиправительственное"),
+            new Criminal("Якушев", "Виктор", "Агафонович", true, "Антиправительственное"),
+            new Criminal("Королёв", "Игорь", "Альвианович", false, "Антиправительственное"),
+            new Criminal("Яковлев", "Ипполит", "Куприянович", false),
+            new Criminal("Ефремов", "Алан", "Александрович", false, "Антиправительственное"),
+            new Criminal("Пономарёва", "Олеся", "Евгеньевна", true),
+            new Criminal("Захаров", "Юстин", "Андреевич", true),
+            new Criminal("Орехов", "Адам", "Валентинович", false),
+            new Criminal("Власова", "Даниэла", "Александровна", false, "Антиправительственное"),
+            new Criminal("Киселёв", "Митрофан", "Арсеньевич", true, "Антиправительственное"),
+            new Criminal("Жданова", "Елизавета", "Наумовна", true, "Антиправительственное")
+        };
+    }
+
+    public void Work()
+    {
+        const string CommandShow = "Показать всех";
+        const string CommandAmnesty = "Помиловать по запросу";
+        const string CommandExit = "Завершить работу";
+
+        string[] menu = new string[]
+        {
+            CommandShow,
+            CommandAmnesty,
+            CommandExit
+        };
+
+        string userInput;
+
+        bool isWorking = true;
+
+        while (isWorking)
+        {
+            Console.Write("\t\tМеню базы данных преступников\n\n");
+
+            Console.Write("Доступные команды:\n");
+            PrintMenu(menu);
+
+            Console.Write("Ожидается ввод: ");
+            userInput = GetCommandMenu(menu);
+
+            Console.Clear();
+
+            switch (userInput)
+            {
+                case CommandShow:
+                    Show();
+                    break;
+
+                case CommandAmnesty:
+                    Amnesty();
+                    break;
+
+                case CommandExit:
+                    Console.Write("Вы завершии работу базы данных.\n\n");
+                    isWorking = false;
+                    continue;
+
+                default:
+                    Console.Write("Требуется ввести номер команды или саму команду.\n");
+                    break;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+
+    private void Show()
+    {
+        int numberCriminal = 1;
+
+        if (_criminals.Count > 0)
+        {
+            foreach (Criminal criminal in _criminals)
+            {
+                Console.Write($"\t{numberCriminal}. ".PadRight(5));
+                criminal.Show();
+
+                numberCriminal++;
+            }
+        }
+        else
+        {
+            Console.Write("Нет ни одной записи.\n");
+        }
+
+        Console.WriteLine();
+    }
+
+    private void Amnesty()
+    {
+        string typeCrime = "Антиправительственное";
+
+        Console.Write("Преступники до амнистии:\n");
+        Show();
+
+        ExecuteAmnesty(typeCrime);
+
+        Console.Write("Преступники после амнистии:\n");
+        Show();
+    }
+
+    private void ExecuteAmnesty(string typeCrime)
+    {
+        List<Criminal> prisonersCriminals;
+        prisonersCriminals = _criminals.Where(criminal => criminal.TypeCrime == typeCrime).ToList();
+
+        foreach (Criminal criminal in prisonersCriminals)
+            criminal.Amnesty(typeCrime);
+    }
+
+    private void PrintMenu(string[] menu)
+    {
+        int numberCommand = 1;
+
+        for (int i = 0; i < menu.Length; i++)
+        {
+            Console.Write($"\t{numberCommand}. {menu[i]}\n");
+            numberCommand++;
+        }
+
+        Console.WriteLine();
+    }
+
+    private string GetCommandMenu(string[] menu)
+    {
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int number))
+            if (number > 0 && number <= menu.Length)
+                return menu[number - 1];
+
+        return userInput;
+    }
+}
+
+public class Criminal
+{
+    private readonly string _lastName;
+    private readonly string _firstName;
+    private readonly string _middleName;
+
+    public Criminal(string lastName, string firstName, string middleName, bool isPrisoner, string typeCrime = null)
+    {
+        _lastName = lastName;
+        _firstName = firstName;
+        _middleName = middleName;
+        IsPrisoner = isPrisoner;
+        TypeCrime = typeCrime;
+    }
+
+    public bool IsPrisoner { get; private set; }
+    public string TypeCrime { get; private set; }
+
+    public void Show()
+    {
+        Console.Write($"{_lastName}".PadRight(15) +
+                      $"{_firstName}".PadRight(15) +
+                      $"{_middleName}".PadRight(15) +
+                      $"Статус: {(IsPrisoner ? "Заключён" : "Свободен")}".PadRight(17) +
+                      $"Тип преступления: {TypeCrime ?? "Отсутствует"}\n");
+    }
+
+    public void Amnesty(string typeCrime)
+    {
+        if (TypeCrime == typeCrime)
+        {
+            TypeCrime = null;
+            IsPrisoner = false;
+        }
+    }
+}
