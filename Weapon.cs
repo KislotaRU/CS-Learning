ef113c2e60d839988e374ca5b868e6de1ce78212
KislotaRU <tsepilov.matvey@mail.ru>
2025-10-13 19:22:22 +0300

    Task (Оружие)

---
+++ Weapon.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    public class Player
+    {
+        public int Health { get; private set; }
+
+        public Player(int health)
+        {
+            Health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            int temporaryHealth;
+
+            if (damage < 0)
+                return;
+
+            temporaryHealth = Health - damage;
+
+            if (temporaryHealth > 0)
+                Health = temporaryHealth;
+            else
+                Health = 0;
+        }
+    }
+
+    public class Weapon
+    {
+        public int Damage { get; private set; }
+        public int Bullets { get; private set; }
+
+        public void Fire(Player player)
+        {
+            player.TakeDamage(Damage);
+            Bullets--;
+        }
+    }
+
+    public class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
