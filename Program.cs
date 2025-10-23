a83f45e1e652d2c0a945810784b62ed5f545adb0
KislotaRU <tsepilov.matvey@mail.ru>
2025-10-23 19:27:02 +0300

    Task (CleanCode_ExampleTask1)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS_JUNIOR
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+        }
+
+        public static int Clamp(int value, int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
+                throw new InvalidOperationException();
+
+            if (value < minValue)
+                return minValue;
+            else if (value > maxValue)
+                return maxValue;
+            else
+                return value;
+        }
+    }
+}
