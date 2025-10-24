67bb7e1d2dfa1394aa000a372f1f7da8b11c0684
KislotaRU <tsepilov.matvey@mail.ru>
2025-10-24 13:14:47 +0300

    Task (CleanCode_ExampleTask10)

--- Program.cs
+++ Program.cs
@@ -7,19 +7,5 @@ namespace CS_JUNIOR
         public static void Main()
         {
         }
-
-        public static int IndexOf(int[] array, int element)
-        {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == element)
-                    return i;
-            }
-
-            return -1;
-        }
     }
 }
---
+++ Weapon.cs
@@ -0,0 +1,15 @@
+﻿namespace CS_JUNIOR
+{
+    internal class Weapon
+    {
+        private const int MinCountBullets = 0;
+
+        private int _bullets;
+
+        public bool CanShoot() =>
+            _bullets > MinCountBullets;
+
+        public void Shoot() =>
+            _bullets--;
+    }
+}
